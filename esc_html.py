37ce7befcf8dbf8ede55f01f714b30a7f3964e9e
mackroid <mackroid@gmail.com>
2014-03-02 21:17:11 +0100

    improved naming

--- esc_html.py
+++ esc_html.py
@@ -10,9 +10,12 @@
 # render your escaped text as the corresponding symbols, 
 # but the grading script will still correctly evaluate it.
 # 
+import cgi
 
 def escape_html(s):
-    return s.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace('"', "&quot;")
+    return cgi.escape(s, quote = "TRUE")
+
+    #return s.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace('"', "&quot;")
 
 print escape_html('kaffe & najs < fisk > "kaffe" hej>')                                                                                                                                                               
                                                                                                                                                                
